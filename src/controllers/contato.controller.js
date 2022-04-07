70a39c8aeaa46d7909feb865bddd6eecd56e0ab4
lucaslc01 <lucaslc01@hotmail.com>
2022-04-07 12:51:01 -0300

    mysql post e correcao get

--- src/controllers/contato.controller.js
+++ src/controllers/contato.controller.js
@@ -10,7 +10,7 @@ var router = express.Router();
 
 //consulta todos os contatos
 router.get("/todosOsContatos",(req,res)=>{
-  connection.query(" select contato.*, group_concat(distinct(telefones.telefone) separator ',') as 'telefone', group_concat(distinct(emails.email) separator ',') as 'email' from ((contato inner join telefones on contato.nome = telefones.nome and contato.sobrenome = telefones.sobrenome) inner join emails on emails.nome = contato.nome and emails.sobrenome = contato.sobrenome)",(err,records,fields)=>{
+  connection.query(" select contato.*, group_concat(distinct(telefones.telefone) separator ',') as 'telefone', group_concat(distinct(emails.email) separator ',') as 'email' from ((contato inner join telefones on contato.nome = telefones.nome and contato.sobrenome = telefones.sobrenome) inner join emails on emails.nome = contato.nome and emails.sobrenome = contato.sobrenome) group by contato.nome, contato.sobrenome",(err,records,fields)=>{
     if(err){
       console.error("Error while fetching data");
     }else{
@@ -42,13 +42,12 @@ router.post("/inserirContato",(req,res)=>{
   var observacao = req.body.observacao;
   var endereco = req.body.endereco;
   var telefone = req.body.telefone;
+  var email = req.body.email;
 
   connection.query("insert into contato values ('"+nome+"','"+sobrenome+"','"+empresa+"','"+cargo+"','"+apelido+"',"+aniversario+", '"+observacao+"', '"+endereco+"')",
   (err,result)=>{
     if(err){
       console.error("Error while inserting contato");
-    }else{
-      res.send({insert:"success contato"});
     }
   })
 
@@ -56,29 +55,19 @@ router.post("/inserirContato",(req,res)=>{
   (err,result)=>{
     if(err){
       console.error("Error while inserting telefone");
-    }else{
-      res.send({insert:"success saving telefone"});
     }
   })
-
-})
-
-
-//salva email do contato
-router.post("/inserirEmail",(req,res)=>{
-
-  var nome = req.body.nome;
-  var sobrenome = req.body.sobrenome;
-  var email = req.body.email;
 
   connection.query("insert into emails values ('"+nome+"','"+sobrenome+"','"+email+"') ",
   (err,result)=>{
     if(err){
       console.error("Error while inserting email");
     }else{
-      res.send({insert:"success saving email"});
+      res.send({insert:"success saving contato, telefone and email"});
     }
   })
+
 })
+
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 
 var app = express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 var contatoAPI = require("./controllers/contato.controller");
 app.use("/api/contatos",contatoAPI);
