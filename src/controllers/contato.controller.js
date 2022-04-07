506fdb09ed3233bb43964733cafd53fada18d70a
lucaslc01 <lucaslc01@hotmail.com>
2022-04-07 10:42:21 -0300

    mysql consulta e posts

--- src/controllers/contato.controller.js
+++ src/controllers/contato.controller.js
@@ -8,8 +8,9 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get("/contato",(req,res)=>{
-  connection.query("select * from contato",(err,records,fields)=>{
+//consulta todos os contatos
+router.get("/todosOsContatos",(req,res)=>{
+  connection.query(" select contato.*, group_concat(distinct(telefones.telefone) separator ',') as 'telefone', group_concat(distinct(emails.email) separator ',') as 'email' from ((contato inner join telefones on contato.nome = telefones.nome and contato.sobrenome = telefones.sobrenome) inner join emails on emails.nome = contato.nome and emails.sobrenome = contato.sobrenome)",(err,records,fields)=>{
     if(err){
       console.error("Error while fetching data");
     }else{
@@ -18,8 +19,10 @@ router.get("/contato",(req,res)=>{
   })
 })
 
-router.get("/telefones",(req,res)=>{
-  connection.query("select * from telefones",(err,records,fields)=>{
+
+//consulta todos os dados de um mesmo contato
+router.get("/buscaContato/:nome/:sobrenome",(req,res)=>{
+  connection.query(" select contato.*, group_concat(distinct(telefones.telefone) separator ',') as 'telefone', group_concat(distinct(emails.email) separator ',') as 'email' from ((contato inner join telefones on contato.nome = telefones.nome and contato.sobrenome = telefones.sobrenome) inner join emails on emails.nome = contato.nome and emails.sobrenome = contato.sobrenome) where contato.nome='"+req.params.nome+"' and contato.sobrenome='"+req.params.sobrenome+"'",(err,records,fields)=>{
     if(err){
       console.error("Error while fetching data");
     }else{
@@ -28,22 +31,52 @@ router.get("/telefones",(req,res)=>{
   })
 })
 
-router.get("/emails",(req,res)=>{
-  connection.query("select * from emails",(err,records,fields)=>{
+//salva contato com telefone (preencher o telefone é obrigatório)
+router.post("/inserirContato",(req,res)=>{
+  var nome = req.body.nome;
+  var sobrenome = req.body.sobrenome;
+  var empresa = req.body.empresa;
+  var cargo = req.body.cargo;
+  var apelido = req.body.apelido;
+  var aniversario = req.body.aniversario;
+  var observacao = req.body.observacao;
+  var endereco = req.body.endereco;
+  var telefone = req.body.telefone;
+
+  connection.query("insert into contato values ('"+nome+"','"+sobrenome+"','"+empresa+"','"+cargo+"','"+apelido+"',"+aniversario+", '"+observacao+"', '"+endereco+"')",
+  (err,result)=>{
     if(err){
-      console.error("Error while fetching data");
+      console.error("Error while inserting contato");
     }else{
-      res.send(records);
+      res.send({insert:"success contato"});
     }
   })
+
+  connection.query("insert into telefones values ('"+nome+"','"+sobrenome+"',"+telefone+")",
+  (err,result)=>{
+    if(err){
+      console.error("Error while inserting telefone");
+    }else{
+      res.send({insert:"success saving telefone"});
+    }
+  })
+
 })
 
-router.get("/buscacontato/:nome/:sobrenome",(req,res)=>{
-  connection.query("select * from contato where nome='"+req.params.nome+"' and sobrenome='"+req.params.sobrenome+"'",(err,records,fields)=>{
+
+//salva email do contato
+router.post("/inserirEmail",(req,res)=>{
+
+  var nome = req.body.nome;
+  var sobrenome = req.body.sobrenome;
+  var email = req.body.email;
+
+  connection.query("insert into emails values ('"+nome+"','"+sobrenome+"','"+email+"') ",
+  (err,result)=>{
     if(err){
-      console.error("Error while fetching data");
+      console.error("Error while inserting email");
     }else{
-      res.send(records);
+      res.send({insert:"success saving email"});
     }
   })
 })
